b17f3b1a436326494d9a0daba27781d714d7978a
Emma B <124230094+emmabbui@users.noreply.github.com>
2025-05-13 13:51:00 -0700

    Initial commit

---
+++ zenbeauty/action_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Appointments</title>
+    <link rel="stylesheet" href="zenbeauty.css" />
+  </head>
+  <body>
+    <div id="wrapper">
+      <header class="gradient">
+        <h1><a href="index.html">Zen Beauty</a></h1>
+      </header>
+      <main>
+        <h2>See You Soon!</h2> <br>
+        Your information: 
+      </main>
+
+</body>
+</html>
